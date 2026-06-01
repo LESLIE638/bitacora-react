486188f1f5d3c95d130bd7f68484bda5dc705543
FRANCISCO ROBLES <lesthyme1@gmail.com>
2026-06-01 17:14:10 -0600

    Variables de entorno Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -12,3 +12,11 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "bitacora-93672.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: "bitacora-93672.firebasestorage.app",
+  messagingSenderId: "310184409931",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
+};
